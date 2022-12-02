0c7dd6c96e9b8bf1d101d312c2937a94b8bd4166
Alex Silva <alexcssilva@gmail.com>
2022-12-02 00:56:47 -0300

    perf: :zap: added one more condition of size of a username

--- database/utils/validateUser.js
+++ database/utils/validateUser.js
@@ -1,10 +1,15 @@
 const { getCpf } = require("../services/userService");
 
 const validateUser = async (req, res, next) => {
-  const { cpf } = req.body;
+  const { cpf, username } = req.body;
   const cpfUser = await getCpf(cpf);
 
   if (cpf.length < 11) {
+    return res.status(400).json({ message: "User with invalid CPF" });
+  }
+
+  if (username.length < 6) {
+    return res.status(400).json({ message: "This field requires a minimum of 6 characters" });
   }
 
   if (cpfUser) {
