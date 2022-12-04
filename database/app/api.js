f834e1f82a959d3567af7da7e97139f358fba836
Alex Silva <alexcssilva@gmail.com>
2022-12-04 17:41:40 -0300

    chore: :arrow_up: added cors for frontend integration

--- database/app/api.js
+++ database/app/api.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 
 const router = require("../router/index");
@@ -5,6 +6,7 @@ const router = require("../router/index");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
