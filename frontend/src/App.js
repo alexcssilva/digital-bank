132d9c897e8a53bb037d7a1fe194e9d1d2b1ddc9
Alex Silva <alexcssilva@gmail.com>
2022-12-04 22:38:56 -0300

    feat: :sparkles: added app.js and index.js files

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Balance from "./pages/Balance";
+import Register from "./pages/Register";
+import Transfer from "./pages/Transfer";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/users/balance" />} />
+          <Route path="/users/register" element={<Register />} />
+          <Route path="/users/balance" element={<Balance />} />
+          <Route path="/users/transfer" element={<Transfer />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
