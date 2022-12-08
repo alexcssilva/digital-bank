4b660c230d7ec933bc33c4a5e4cda06579495083
Alex Silva <alexcssilva@gmail.com>
2022-12-08 18:29:06 -0300

    fix: :triangular_flag_on_post: Deleted the first folders that were structured

--- database/router/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const userRouter = require("./user/userRouter");
-
-const router = express();
-
-router.use("/users", userRouter);
-
-module.exports = router;
--- tests/assets/constants.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  sequelize: {
-    drop: "npx sequelize-cli db:drop",
-    create: "npx sequelize-cli db:create",
-    migrate: "npx sequelize-cli db:migrate",
-  },
-  apiURL: "http://localhost:3001",
-};
